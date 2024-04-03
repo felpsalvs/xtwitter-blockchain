ba22a85bacc06cefa637f588cf983c0d8610dcb3
felpsalvs <profelipealves@outlook.com>
2024-04-03 18:15:03 -0300

    chore: added first post

--- x-smart-contracts/scripts/run.js
+++ x-smart-contracts/scripts/run.js
@@ -1,7 +1,17 @@
 const main = async () => {
+  const [owner] = await hre.ethers.getSigners();
   const xPost = await hre.ethers.deployContract("XPost");
   await xPost.waitForDeployment();
+  
   console.log("Deploy do contrato no endereço:", xPost.target);
+  console.log("Deploy do contrato feito por:", owner.address);
+
+  await xPost.getTotalPosts();
+
+  const waveTxn = await xPost.createPost();
+  await waveTxn.wait();
+
+  await xPost.getTotalPosts();
 };
 
 const runMain = async () => {
