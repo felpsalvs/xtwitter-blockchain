575b1f01bea12e64b214776e11f475aeb3e60c0f
felpsalvs <profelipealves@outlook.com>
2024-04-03 17:02:01 -0300

    first commit

---
+++ x-smart-contracts/contracts/XPost.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract XPost {
+    constructor(){
+        console.log("Eu serei o contrato inteligente para fazer um post no Twitter descentralizado.");
+    }
+}
